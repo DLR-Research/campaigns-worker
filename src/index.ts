e048521495926ef53cdb6347894248c2e2944265
Lawrence Wu <llwu@llwu.me>
2021-10-12 03:03:49 -0700

    undo cors 'fix'

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,5 @@ router.get('/campaigns/:campaignId/stats/:userId', getStats)
 router.all('*', () => new Response('404 Not Found', { status: 404 }))
 
 addEventListener('fetch', e => {
-  const response = router.handle(e.request)
-  response.headers.set("Access-Control-Allow-Origin", "https://localhost:8080")
-  response.headers.append("Vary", "Origin")
-  e.respondWith(response)
+  e.respondWith(router.handle(e.request))
 })
