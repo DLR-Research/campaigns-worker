d0e27f19aa2f0c3280476dbbc8349132d1381082
Lawrence Wu <llwu@llwu.me>
2021-10-12 00:42:33 -0700

    ts

--- src/db.ts
+++ src/db.ts
@@ -1,18 +1,34 @@
 import { RDSDataClient, ExecuteStatementCommand } from '@aws-sdk/client-rds-data'
 
-export const AWS_OPTS = {
-  region: AWS_REGION,
-  credentials: {
-    accessKeyId: AWS_ACCESS_KEY_ID,
-    secretAccessKey: AWS_SECRET_ACCESS_KEY
-  }
+interface AwsCredentials {
+  accessKeyId: string
+  secretAccessKey: string
 }
 
-export const sql = query =>
+interface AwsOpts {
+  region: string
+  credentials: AwsCredentials
+}
+
+export const AWS_OPTS = {
+  // @ts-ignore
+  region: AWS_REGION,
+  credentials: {
+    // @ts-ignore
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    // @ts-ignore
+    secretAccessKey: AWS_SECRET_ACCESS_KEY
+  }
+} as AwsOpts
+
+export const sql = (query: string) =>
   new ExecuteStatementCommand({
+    // @ts-ignore
     database: AWS_DB_NAME,
     sql: query,
+    // @ts-ignore
     resourceArn: AWS_AURORA_RESOURCE_ARN,
+    // @ts-ignore
     secretArn: AWS_AURORA_SECRET_ARN
   })
 
